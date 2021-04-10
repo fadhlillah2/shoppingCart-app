ba874ce8069fd9002e785f4bebe7c514a7366b69
fadhlillah2 <fadhlillah949699@gmail.com>
2021-04-11 04:40:48 +0700

    add updateCartTotal function

--- store.js
+++ store.js
@@ -5,5 +5,24 @@ for (var i = 0; i < removeCartItemButtons.length; i++) {
   button.addEventListener("click", function (event) {
     var buttonClicked = event.target;
     buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal();
   });
+}
+
+function updateCartTotal() {
+  var cartItemContainer = document.getElementsByClassName("cart-items")[0];
+  var cartRows = cartItemContainer.getElementsByClassName("cart-row");
+  var total = 0;
+  for (var i = 0; i < cartRows.length; i++) {
+    var cartRow = cartRows[i];
+    var priceElement = cartRow.getElementsByClassName("cart-price")[0];
+    var quantityElement = cartRow.getElementsByClassName(
+      "cart-quantity-input"
+    )[0];
+    var price = parseFloat(priceElement.innerText.replace("$", ""));
+    var quantity = quantityElement.value;
+    total = total + price * quantity;
+  }
+  document.getElementsByClassName("cart-total-price")[0].innerText =
+    "$" + total;
 }

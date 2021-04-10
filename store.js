a1c228b7fae4a05f92a740ebf44e2c9b219aa245
fadhlillah2 <fadhlillah949699@gmail.com>
2021-04-11 06:55:32 +0700

    quantity input is now working

--- store.js
+++ store.js
@@ -76,6 +76,9 @@ function addItemToCart(title, price, imageSrc) {
   cartRow
     .getElementsByClassName("btn-danger")[0]
     .addEventListener("click", removeCartItem);
+  cartRow
+    .getElementsByClassName("cart-quantity-input")[0]
+    .addEventListener("change", quantityChanged);
 }
 
 function updateCartTotal() {

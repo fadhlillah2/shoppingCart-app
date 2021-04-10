3b34c625b9bef7754511dec2822f01f0cb7c40f6
fadhlillah2 <fadhlillah949699@gmail.com>
2021-04-11 05:49:26 +0700

    create funtion for quantity have absolute value (cannot be negative)

--- store.js
+++ store.js
@@ -11,11 +11,25 @@ function ready() {
     var button = removeCartItemButtons[i];
     button.addEventListener("click", removeCartItem);
   }
+
+  var quantityInputs = document.getElementsByClassName("cart-quantity-input");
+  for (var i = 0; i < quantityInputs.length; i++) {
+    var input = quantityInputs[i];
+    input.addEventListener("change", quantityChanged);
+  }
 }
 
 function removeCartItem(event) {
   var buttonClicked = event.target;
   buttonClicked.parentElement.parentElement.remove();
+  updateCartTotal();
+}
+
+function quantityChanged(event) {
+  var input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
   updateCartTotal();
 }
 

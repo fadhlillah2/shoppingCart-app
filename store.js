bf1487518db91874cc4e3100bed94aa3678844c3
fadhlillah2 <fadhlillah949699@gmail.com>
2021-04-11 06:35:49 +0700

    addToCart button is active but quantity still cannot be used

--- store.js
+++ store.js
@@ -43,7 +43,28 @@ function addToCartClicked(event) {
   var button = event.target;
   var shopItem = button.parentElement.parentElement;
   var title = shopItem.getElementsByClassName("shop-item-title")[0].innerText;
-  console.log(title);
+  var price = shopItem.getElementsByClassName("shop-item-price")[0].innerText;
+  var imageSrc = shopItem.getElementsByClassName("shop-item-image")[0].src;
+  console.log(title, price, imageSrc);
+  addItemToCart(title, price, imageSrc);
+}
+
+function addItemToCart(title, price, imageSrc) {
+  var cartRow = document.createElement("div");
+  cartRow.classList.add("cart-row");
+  var cartItems = document.getElementsByClassName("cart-items")[0];
+  var cartRowContents = `
+  <div class="cart-item cart-column">
+                        <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+                        <span class="cart-item-title">${title}</span>
+                    </div>
+                    <span class="cart-price cart-column">${price}</span>
+                    <div class="cart-quantity cart-column">
+                        <input class="cart-quantity-input" type="number" value="1">
+                        <button class="btn btn-danger" type="button">REMOVE</button>
+                    </div>`;
+  cartRow.innerHTML = cartRowContents;
+  cartItems.append(cartRow);
 }
 
 function updateCartTotal() {

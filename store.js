89be2fddab1629bdd3d877c68bae333ef1981adf
fadhlillah2 <fadhlillah949699@gmail.com>
2021-04-10 22:59:18 +0700

    create remove function

--- store.js
+++ store.js
@@ -1,11 +1,9 @@
 var removeCartItemButtons = document.getElementsByClassName("btn-danger");
 console.log(removeCartItemButtons);
-for (var i = 0; i < removeCartItemButtons; i++) {
+for (var i = 0; i < removeCartItemButtons.length; i++) {
   var button = removeCartItemButtons[i];
   button.addEventListener("click", function (event) {
     var buttonClicked = event.target;
-    button.buttonClicked.parentElement.parentElement.remove();
+    buttonClicked.parentElement.parentElement.remove();
   });
 }
-
-console.log("er");

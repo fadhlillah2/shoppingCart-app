a7914e79ce90eab6187298e520fe9c077d8755d1
fadhlillah2 <fadhlillah949699@gmail.com>
2021-04-10 09:16:34 +0700

    add index.html

--- store.js
+++ store.js
@@ -1,1 +1,11 @@
-console.log("here");
+var removeCartItemButtons = document.getElementsByClassName("btn-danger");
+console.log(removeCartItemButtons);
+for (var i = 0; i < removeCartItemButtons; i++) {
+  var button = removeCartItemButtons[i];
+  button.addEventListener("click", function (event) {
+    var buttonClicked = event.target;
+    button.buttonClicked.parentElement.parentElement.remove();
+  });
+}
+
+console.log("er");

cdda9392b09a8f8c00405539ad861bb907ae074a
fadhlillah2 <fadhlillah949699@gmail.com>
2021-04-11 07:07:43 +0700

    purchase button activated

--- store.js
+++ store.js
@@ -23,6 +23,19 @@ function ready() {
     var button = addToCartButtons[i];
     button.addEventListener("click", addToCartClicked);
   }
+
+  document
+    .getElementsByClassName("btn-purchase")[0]
+    .addEventListener("click", purchaseClicked);
+}
+
+function purchaseClicked() {
+  alert("Thank you for your purchase");
+  var cartItems = document.getElementsByClassName("cart-items")[0];
+  while (cartItems.hasChildNodes()) {
+    cartItems.removeChild(cartItems.firstChild);
+  }
+  updateCartTotal();
 }
 
 function removeCartItem(event) {
